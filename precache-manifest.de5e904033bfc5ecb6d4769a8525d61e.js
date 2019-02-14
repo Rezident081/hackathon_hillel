487e2ec7e171b593fff087b771ef6a73266c8be5
Maxim <k.maxim107@gmail.com>
2019-02-15 00:13:35 +0200

    Updates

---
+++ precache-manifest.de5e904033bfc5ecb6d4769a8525d61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/hackathon_hillel/static/media/chucknorris_logo_coloured_small@2x.39b94c0e.png"
+  },
+  {
+    "revision": "7d5c61ff118838e0ad49",
+    "url": "/hackathon_hillel/static/js/runtime~main.7d5c61ff.js"
+  },
+  {
+    "revision": "3cd37ab21a2b5a214a94",
+    "url": "/hackathon_hillel/static/js/main.3cd37ab2.chunk.js"
+  },
+  {
+    "revision": "85a72b250cf89c2ee504",
+    "url": "/hackathon_hillel/static/js/2.85a72b25.chunk.js"
+  },
+  {
+    "revision": "3cd37ab21a2b5a214a94",
+    "url": "/hackathon_hillel/static/css/main.53a9671a.chunk.css"
+  },
+  {
+    "revision": "2c38a32f120c6a047e22391a48baa06b",
+    "url": "/hackathon_hillel/index.html"
+  }
+];
